90e93c7de9bac460d746ce2a62f349cf39e78a15
iKittyCode <kids@Himanis-MacBook-Pro.local>
2020-11-12 20:08:47 -0500

    s

---
+++ index.js
@@ -0,0 +1,6 @@
+var names = ['Chooni', 'Daddy', 'Mommy', 'Arshya', 'Kittu']
+setInterval(() => 
+    {
+    console.log(names[Math.floor(Math.random() * names.length)])
+    }, 1000)
+    console.log("❤️")
